0602ce0bb16538a096538298a665f4636452e09f
Gabriel Bontorin <gabriel@studiobnt.com.br>
2015-01-28 17:35:04 -0200

    Retirei o site de 'One Page Layout' e adicionei as demais páginas...

--- empresa.php
+++ empresa.php
@@ -40,8 +40,8 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="index.php">Home</a></li>
-        <li><a href="empresa.php">Empresa</a></li>
+        <li><a href="index.php">Home</a></li>
+        <li class="active"><a href="empresa.php">Empresa</a></li>
 		<li><a href="produtos.php">Produtos</a></li>
         <li><a href="servicos.php">Serviços</a></li>
 		<li><a href="#contato">Contato</a></li>
@@ -50,23 +50,100 @@
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container -->
-    </nav>
+    </nav>	
 
-	<!-- Section: intro -->
+    <!-- Section: intro -->
     <section id="intro" class="intro">
 	
 		<div class="slogan">
-			<h2>BEM VINDO AO <span class="text_color">MEU PROJETO</span> </h2>
-			<h4>ESTE PROJETO FOI DESENVOLVIDO COM TWITTER BOOTSTRAP 3</h4>
+			<h2>ESTA É A PÁGINA <span class="text_color">EMPRESA</span> </h2>
+			<h4>CLIQUE NO BOTÃO ABAIXO PARA IR ATÉ O CONTEÚDO</h4>
 		</div>
 		<div class="page-scroll">
-			<a href="#contato" class="btn btn-circle">
+			<a href="#empresa" class="btn btn-circle">
 				<i class="fa fa-angle-double-down animated"></i>
 			</a>
 		</div>
     </section>
 	<!-- /Section: intro -->
+    
+	<!-- Section: empresa -->
+    <section id="empresa" class="home-section text-center">
+		<div class="heading-about">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="wow bounceInDown" data-wow-delay="0.4s">
+					<div class="section-heading">
+					<h2>Empresa</h2>
+					<i class="fa fa-2x fa-angle-down"></i>
 
+					</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="container">
+
+		<div class="row">
+			<div class="col-lg-2 col-lg-offset-5">
+				<hr class="marginbot-50">
+			</div>
+		</div>
+        <div class="row">
+            <div class="col-xs-6 col-sm-3 col-md-3">
+				<div class="wow bounceInUp" data-wow-delay="0.2s">
+                <div class="team boxed-grey">
+                    <div class="inner">
+						<h5>Anna</h5>
+                        <p class="subtitle">Pixel Crafter</p>
+                        <div class="avatar"><img src="img/team/1.jpg" alt="" class="img-responsive img-circle" /></div>
+                    </div>
+                </div>
+				</div>
+            </div>
+			<div class="col-xs-6 col-sm-3 col-md-3">
+				<div class="wow bounceInUp" data-wow-delay="0.5s">
+                <div class="team boxed-grey">
+                    <div class="inner">
+						<h5>Maura</h5>
+                        <p class="subtitle">Ruby on Rails</p>
+                        <div class="avatar"><img src="img/team/2.jpg" alt="" class="img-responsive img-circle" /></div>
+
+                    </div>
+                </div>
+				</div>
+            </div>
+			<div class="col-xs-6 col-sm-3 col-md-3">
+				<div class="wow bounceInUp" data-wow-delay="0.8s">
+                <div class="team boxed-grey">
+                    <div class="inner">
+						<h5>Jack</h5>
+                        <p class="subtitle">jQuery Ninja</p>
+                        <div class="avatar"><img src="img/team/3.jpg" alt="" class="img-responsive img-circle" /></div>
+
+                    </div>
+                </div>
+				</div>
+            </div>
+			<div class="col-xs-6 col-sm-3 col-md-3">
+				<div class="wow bounceInUp" data-wow-delay="1s">
+                <div class="team boxed-grey">
+                    <div class="inner">
+						<h5>Tom</h5>
+                        <p class="subtitle">Typographer</p>
+                        <div class="avatar"><img src="img/team/4.jpg" alt="" class="img-responsive img-circle" /></div>
+
+                    </div>
+                </div>
+				</div>
+            </div>
+        </div>		
+		</div>
+	</section>
+	<!-- /Section: empresa -->
+	
 	<!-- Section: contato -->
     <section id="contato" class="home-section text-center bg-dark">
 		<div class="heading-contact">
--- servicos.php
+++ servicos.php
@@ -40,10 +40,10 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="empresa.php">Empresa</a></li>
 		<li><a href="produtos.php">Produtos</a></li>
-        <li><a href="servicos.php">Serviços</a></li>
+        <li class="active"><a href="servicos.php">Serviços</a></li>
 		<li><a href="#contato">Contato</a></li>
       </ul>
             </div>
@@ -51,21 +51,155 @@
         </div>
         <!-- /.container -->
     </nav>
-
-	<!-- Section: intro -->
+    
+    <!-- Section: intro -->
     <section id="intro" class="intro">
 	
 		<div class="slogan">
-			<h2>BEM VINDO AO <span class="text_color">MEU PROJETO</span> </h2>
-			<h4>ESTE PROJETO FOI DESENVOLVIDO COM TWITTER BOOTSTRAP 3</h4>
+			<h2>ESTA É A PÁGINA <span class="text_color">SERVIÇOS</span> </h2>
+			<h4>CLIQUE NO BOTÃO ABAIXO PARA IR ATÉ O CONTEÚDO</h4>
 		</div>
 		<div class="page-scroll">
-			<a href="#contato" class="btn btn-circle">
+			<a href="#servicos" class="btn btn-circle">
 				<i class="fa fa-angle-double-down animated"></i>
 			</a>
 		</div>
     </section>
 	<!-- /Section: intro -->
+    
+    <!-- Section: serviços -->
+    <section id="servicos" class="home-section text-center">
+		
+		<div class="heading-about">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="wow bounceInDown" data-wow-delay="0.4s">
+					<div class="section-heading">
+					<h2>Serviços</h2>
+					<i class="fa fa-2x fa-angle-down"></i>
+
+					</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="container">
+		<div class="row">
+			<div class="col-lg-2 col-lg-offset-5">
+				<hr class="marginbot-50">
+			</div>
+		</div>
+        <div class="row">
+            <div class="col-sm-3 col-md-3">
+				<div class="wow fadeInLeft" data-wow-delay="0.2s">
+                <div class="service-box">
+					<div class="service-icon">
+						<img src="img/icons/service-icon-1.png" alt="" />
+					</div>
+					<div class="service-desc">
+						<h5>Print</h5>
+						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+					</div>
+                </div>
+				</div>
+            </div>
+			<div class="col-sm-3 col-md-3">
+				<div class="wow fadeInUp" data-wow-delay="0.2s">
+                <div class="service-box">
+					<div class="service-icon">
+						<img src="img/icons/service-icon-2.png" alt="" />
+					</div>
+					<div class="service-desc">
+						<h5>Web Design</h5>
+						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+					</div>
+                </div>
+				</div>
+            </div>
+			<div class="col-sm-3 col-md-3">
+				<div class="wow fadeInUp" data-wow-delay="0.2s">
+                <div class="service-box">
+					<div class="service-icon">
+						<img src="img/icons/service-icon-3.png" alt="" />
+					</div>
+					<div class="service-desc">
+						<h5>Fotografia</h5>
+						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+					</div>
+                </div>
+				</div>
+            </div>
+			<div class="col-sm-3 col-md-3">
+				<div class="wow fadeInRight" data-wow-delay="0.2s">
+                <div class="service-box">
+					<div class="service-icon">
+						<img src="img/icons/service-icon-4.png" alt="" />
+					</div>
+					<div class="service-desc">
+						<h5>Cloud</h5>
+						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+					</div>
+                </div>
+				</div>
+            </div>
+            <div class="col-sm-3 col-md-3">
+				<div class="wow fadeInLeft" data-wow-delay="0.2s">
+                <div class="service-box">
+					<div class="service-icon">
+						<img src="img/icons/service-icon-1.png" alt="" />
+					</div>
+					<div class="service-desc">
+						<h5>Print</h5>
+						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+					</div>
+                </div>
+				</div>
+            </div>
+			<div class="col-sm-3 col-md-3">
+				<div class="wow fadeInUp" data-wow-delay="0.2s">
+                <div class="service-box">
+					<div class="service-icon">
+						<img src="img/icons/service-icon-2.png" alt="" />
+					</div>
+					<div class="service-desc">
+						<h5>Web Design</h5>
+						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+					</div>
+                </div>
+				</div>
+            </div>
+			<div class="col-sm-3 col-md-3">
+				<div class="wow fadeInUp" data-wow-delay="0.2s">
+                <div class="service-box">
+					<div class="service-icon">
+						<img src="img/icons/service-icon-3.png" alt="" />
+					</div>
+					<div class="service-desc">
+						<h5>Fotografia</h5>
+						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+					</div>
+                </div>
+				</div>
+            </div>
+			<div class="col-sm-3 col-md-3">
+				<div class="wow fadeInRight" data-wow-delay="0.2s">
+                <div class="service-box">
+					<div class="service-icon">
+						<img src="img/icons/service-icon-4.png" alt="" />
+					</div>
+					<div class="service-desc">
+						<h5>Cloud</h5>
+						<p>Vestibulum tincidunt enim in pharetra malesuada. Duis semper magna metus electram accommodare.</p>
+					</div>
+                </div>
+				</div>
+            </div>
+        </div>		
+		</div>
+	</section>
+	<!-- /Section: serviços -->	
 
 	<!-- Section: contato -->
     <section id="contato" class="home-section text-center bg-dark">
